bdb414328143a55da18c1f345b73528ce998492b
LuisAcevedo777 <japay01@hotmail.com>
2022-12-08 18:17:16 -0500

    change three

--- assets/main.js
+++ assets/main.js
@@ -16,7 +16,7 @@ const minusBtn = document.getElementById("minus")
 
 //eliminar
 
-const eliminar = document.getElementById("cart-content")
+const deleteUnited = document.getElementById("cart-content")
 const caneca = document.getElementById("caneca")
 
 const checkBtn = document.getElementById("check")
@@ -83,11 +83,11 @@ const loadComponent = () => {
   //INSTRUCCIONES
   //LO QUE QUEREMOS LLEVAR A CABO
   loader.classList.add("hide")
-  } , 3000)                            
+  } , 0)                            
    } 
   document.addEventListener('DOMContentLoaded', ()=>{
 
-
+     getStorageInfo()
       loadComponent()
   })
 
@@ -124,108 +124,93 @@ darkThemeChange()
 
 
 
- //AGREGAR A CARRITO------------------------------------------------------
+//-----------------AGREGAR A CARRITO--2----------------------------------------------------
+
+const getStorageInfo = ()=>{
+
+let cart = window.localStorage.getItem("cartProduct")
+if(cart){
+
+cart = JSON.parse(cart)
+//MOSTRAR PRODUCTOS EN VENTANA--------------------------------------------------
+
+  const content = document.getElementById("cart-content2") 
+  content.classList.add('cart-content2')
+  let cadena =""
+  cart.map(product => {
+         var ide = product.id
+        
+         
+  cadena +=  `<div class="art-class"><div class="photo" id="cart-content">
+  <img class="featured" src="${product.image}" alt="">
+  <span class="id">id: ${product.id}</span></div>
+  <div class="description2">
+  <span>${product.name}</span>
+
+  <div class="line2"><span>stock:${product.quantity}</span>
+  <div class="barra2"></div>
+  <span class="price2">$${product.price}</span>
+  </div>
+  <span class="sub">Subtotal:$ ${product.price * product.quantitySelected}</span>
+  <div class="plus-minus"><span class="minus" id="minus">-</span>
+  <span>${product.quantitySelected} units</span>
+  <span class="pl" id="pl">+</span>
+  <img class="caneca" id="caneca" src="./images/household_chores_taking_out_trash_garbage_can_icon_133380.svg" alt=""></div>
+  </div></div>`
+
+ 
+  content.innerHTML = cadena
+  
+  })
 
 
-let cartProduct =[];
-    let acc = 0;     
-  function addProduct(itemId){
-    
+
+} else{window.localStorage.setItem("cartProduct", JSON.stringify([]))}}
+
+
+//AGREGAR A CARRITO------------------------------------------------------
+
+
+function addProduct(itemId){
+  let acc=0;
+let cartActual = JSON.parse(window.localStorage.getItem("cartProduct"))
+
+
     //condicion para saber si se puede agregar otro producto
 
-  let productSelected = cartProduct.find(product => product.id === itemId)
+  let productSelected = cartActual.find(product => product.id === itemId)
 
    if(productSelected){
-  let index = cartProduct.indexOf(productSelected)
+  let index = cartActual.indexOf(productSelected)
  
-  cartProduct[index].quantitySelected++
+  cartActual[index].quantitySelected++
   acc = acc + 1
   stockW.textContent = acc
  
-      if(cartProduct[index].quantitySelected > cartProduct[index].quantity){
-        cartProduct[index].quantitySelected = cartProduct[index].quantity
+      if(cartActual[index].quantitySelected > cartActual[index].quantity){
+        cartActual[index].quantitySelected = cartActual[index].quantity
                        
       }
 
    }else{
-      const item = items.find(item=> item.id ===itemId)
-    item.quantitySelected = 1;
-    cartProduct.push(item)
+   
+      const item = items.find(product=> product.id ===itemId)
+      item.quantitySelected = 1;
+    cartActual.push(item)
     acc  = acc+1
     stockW.textContent = acc
     
    } 
-   showProducts()
-    console.log(cartProduct)
-  }
+       console.log(cartActual)
+  
+  window.localStorage.setItem("cartProduct",JSON.stringify(cartActual))
+  getStorageInfo()
+}
 
-
-//MOSTRAR PRODUCTOS EN VENTANA--------------------------------------------------
-
-
-  function showProducts(){
-
-    
-    const content = document.getElementById("cart-content2") 
-    content.classList.add('cart-content2')
-    let cadena =""
-    cartProduct.map(product => {
-           var ide = product.id
-          
-           
-    cadena +=  `<div class="art-class"><div class="photo" id="cart-content">
-    <img class="featured" src="${product.image}" alt="">
-    <span class="id">id: ${product.id}</span></div>
-    <div class="description2">
-    <span>${product.name}</span>
-
-    <div class="line2"><span>stock:${product.quantity}</span>
-    <div class="barra2"></div>
-    <span class="price2">$${product.price}</span>
-    </div>
-    <span class="sub">Subtotal:$ ${product.price * product.quantitySelected}</span>
-    <div class="plus-minus"><span class="minus" id="minus">-</span>
-    <span>${product.quantitySelected} units</span>
-    <span class="pl" id="pl">+</span>
-    <img class="caneca" id="caneca" src="./images/household_chores_taking_out_trash_garbage_can_icon_133380.svg" alt=""></div>
-    </div></div>`
- 
-   
-    content.innerHTML = cadena
-    
-    })
-      } 
-
-
-
-
-
-
-
-//---------------------FUNCTIONS----------------------------//
-
-
-
-
-//aparecer desaparecer ventana carrito
-
-cartBtnOpen.addEventListener("click", e=> cartContainer.classList.remove("hide"))
-
-cartBtnClose.addEventListener("click", e=> cartContainer.classList.add("hide") )
-
-//aparecer desaparecer menu
-
-menuBtnOpen.addEventListener("click", e=> menuContainer.classList.remove("hide"))
-
-menuBtnClose.addEventListener("click", e=> menuContainer.classList.add("hide") )
-
-
-// evento plus
-
+// FUNCTION SUM  Y MIN  Reflejada en ventana de carrito
 
 let counter=0; 
 
-  
 
 function sum(e){
   counter = counter+1
@@ -241,17 +226,43 @@ function sum(e){
 
     }
 
+function eliminar(){
+  
+  window.localStorage.clear()
+  const content = document.getElementById("cart-content2") 
+  content.innerHTML=""
+}
+
+
+
+
+//---------------------EVENTOS----------------------------//
+
+
+//aparecer desaparecer ventana carrito
+
+cartBtnOpen.addEventListener("click", e=> cartContainer.classList.remove("hide"))
+
+cartBtnClose.addEventListener("click", e=> cartContainer.classList.add("hide") )
+
+//aparecer desaparecer menu
+
+menuBtnOpen.addEventListener("click", e=> menuContainer.classList.remove("hide"))
+
+menuBtnClose.addEventListener("click", e=> menuContainer.classList.add("hide") )
+
+
+// evento plus Reflejado en ventana de Carrito 
     
 clickPlus.addEventListener("click", ()=> sum(1) )
 clickPlus2.addEventListener("click", ()=> sum(2) )
 clickPlus3.addEventListener("click", ()=> sum(3) )
     
-//eliminar
+//eliminar TODO
 
-checkBtn.addEventListener("click",()=>{
+checkBtn.addEventListener("click", ()=> {
+  eliminar() })
 
-  window.localStorage.removeItem("propiedad")
-})
 
  
   
@@ -288,10 +299,105 @@ for(let i=0; i<cartProduct.length;i++){
 
 }*/
         
-       
+    
+  //AGREGAR A CARRITO 3
+/*const getStorageInfo = ()=>{
+
+  let cartProduct = window.localStorage.getItem("cartProduct")
+  if(cartProduct){
+  
+  cartProduct = JSON.parse(cartProduct)
+  console.log(cartProduct)
+  
+  }else{
+    window.localStorage.setItem("cartProduct", JSON.stringify([]))
+  }
+  }
+  
+  const addProduct = (producto) => {
+  let cartActual = JSON.parse(window.localStorage.getItem("cartProduct"))
+  
+  
+    cartActual.push(producto)
+    window.localStorage.setItem("cartProducto",JSON,stringify(cartActual))
+  }
+*/
+
+//MOSTRAR PRODUCTOS EN VENTANA--------------------------------------------------
+
+
+  /*function showProducts(){
 
     
-  
+    const content = document.getElementById("cart-content2") 
+    content.classList.add('cart-content2')
+    let cadena =""
+    cartProduct.map(product => {
+           var ide = product.id
+          
+           
+    cadena +=  `<div class="art-class"><div class="photo" id="cart-content">
+    <img class="featured" src="${product.image}" alt="">
+    <span class="id">id: ${product.id}</span></div>
+    <div class="description2">
+    <span>${product.name}</span>
+
+    <div class="line2"><span>stock:${product.quantity}</span>
+    <div class="barra2"></div>
+    <span class="price2">$${product.price}</span>
+    </div>
+    <span class="sub">Subtotal:$ ${product.price * product.quantitySelected}</span>
+    <div class="plus-minus"><span class="minus" id="minus">-</span>
+    <span>${product.quantitySelected} units</span>
+    <span class="pl" id="pl">+</span>
+    <img class="caneca" id="caneca" src="./images/household_chores_taking_out_trash_garbage_can_icon_133380.svg" alt=""></div>
+    </div></div>`
+ 
+   
+    content.innerHTML = cadena
+    
+    })
+      } */
+
+
+
+ //AGREGAR A CARRITO------------------------------------------------------
+
+/*
+let cartProduct =[];
+    let acc = 0;     
+  function addProduct(itemId){
+    
+    //condicion para saber si se puede agregar otro producto
+
+  let productSelected = cartProduct.find(product => product.id === itemId)
+
+   if(productSelected){
+  let index = cartProduct.indexOf(productSelected)
+ 
+  cartProduct[index].quantitySelected++
+  acc = acc + 1
+  stockW.textContent = acc
+ 
+      if(cartProduct[index].quantitySelected > cartProduct[index].quantity){
+        cartProduct[index].quantitySelected = cartProduct[index].quantity
+                       
+      }
+
+   }else{
+      const item = items.find(item=> item.id ===itemId)
+    item.quantitySelected = 1;
+    cartProduct.push(item)
+    acc  = acc+1
+    stockW.textContent = acc
+    
+   } 
+   showProducts()
+    console.log(cartProduct)
+  }*/
+
+
+
 
 
       
